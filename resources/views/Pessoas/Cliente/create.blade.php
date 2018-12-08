0a1539b6db4730a29ff3df0f329ed050657b7a2e
Ezequias Moreira <ezequiasmoreira@hotmail.com>
2018-12-08 00:12:38 -0200

    Criado estrutura do layout da aplicação

---
+++ resources/views/Pessoas/Cliente/create.blade.php
@@ -0,0 +1,5 @@
+@extends("Template.app")
+@section("content")
+
+@endsection
+
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,16 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::group(["prefix" => "cliente", "namespace"=>"pessoas"], function () {
+    Route::get("/{id}/editar", "ClienteController@editarView");
+    Route::get("/{id}/excluir", "ClienteController@excluir");
+    Route::get("/", "ClienteController@index");
+    Route::get("/novo", "ClienteController@novoView");
+    Route::post("/salvar", "ClienteController@salvar");
+    Route::post("/atualizar", "ClienteController@atualizar");
+    Route::post("/retorna-cidade", "ClienteController@retornaCidade");
+    Route::post("/retorna-clientes", "ClienteController@retornaCientes");
 });
